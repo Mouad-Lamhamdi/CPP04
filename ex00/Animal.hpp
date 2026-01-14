9016e4b3e3a747412fdcd0c5cbc9e319f3dc9fdd
Mouad Lamhamdi <molamham@e1r10p2.1337.ma>
2026-01-14 04:20:47 +0100

    ll

---
+++ ex00/Animal.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: molamham <molamham@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/14 04:15:43 by molamham          #+#    #+#             */
+/*   Updated: 2026/01/14 04:20:34 by molamham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+#include <iostream>
+
+class Animal{
+    protected:
+        std::string type;
+
+    public:
+};
+
+#endif
